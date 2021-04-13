3ee4a59719a756c4fa90c4869d535a7905bd1e09
OsvaldoCruzDeLaCruz <osvaldocruzdelacruz2001@gmail.com>
2021-04-13 01:28:42 -0500

    v3

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -61,29 +61,29 @@ const valorCarta = (carta) => {
 //turno de la computadora 
 const turnoComputadora = (puntosMinimos) => {
 
-    do {
-
-        const carta = pedirCarta();
-        puntosComputadora = puntosComputadora + valorCarta(carta);
-        puntosHTML[1].innerText = puntosComputadora;
-
-        const imgCarta = document.createElement('img');
-        imgCarta.src = `assets/cartas/${carta}.png`;
-        imgCarta.classList.add('carta')
-
-        divCartasComputadora.append(imgCarta);
-
-        if (puntosMinimos > 21) {
-            break;
-        }
-        // else if (puntosMinimos === 21) {
-        //     break;
-        // }
+    while ((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
 
 
+    const carta = pedirCarta();
+    puntosComputadora = puntosComputadora + valorCarta(carta);
+    puntosHTML[1].innerText = puntosComputadora;
+
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${carta}.png`;
+    imgCarta.classList.add('carta')
+
+    divCartasComputadora.append(imgCarta);
+
+    if (puntosMinimos > 21) {
+        break;
+    }
+    else if (puntosMinimos === 21) {
+        break;
+    }
 
 
-    } while ((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
+
+
 
     setTimeout(() => {
 
